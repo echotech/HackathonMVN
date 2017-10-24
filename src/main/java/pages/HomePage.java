670884f17991d5156bf0e3659bed5f53ef2d0c94
Jed Reisner <djechotech@gmail.com>
2017-10-24 15:27:15 -0600

    Added tests 3 and 4. 4 is incomplete.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,8 +2,11 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 
 public class HomePage {
@@ -14,8 +17,17 @@ public class HomePage {
         this.driver = driver;
     }
 
-    @FindBy(xpath = "//*[@id='top_menu']/ul/li[3]/a")
+    @FindBy(xpath = "//div[@id='top_menu']/ul/li[3]/a")
     WebElement storiesLink;
+
+    @FindBy(xpath = "//div[@id='top_menu']/ul")
+    List<WebElement> subMenu;
+
+    @FindBy(xpath = "//div[@id=\"top_menu\"]/ul/li[2]/a")
+    WebElement resortsHover;
+
+    @FindBy(xpath = "//div[@id=\"top_menu\"]/ul/li[2]/ul/li[1]/ul")
+    List<WebElement> resortsList;
 
     public void clickStories() {
         storiesLink.click();
@@ -23,6 +35,34 @@ public class HomePage {
 
     public String getTitle() {
         return driver.getTitle().toString();
+    }
+
+    public void getSubMenu() {
+        for (WebElement we : subMenu) {
+            Actions builder = new Actions(driver);
+            Actions hoverOver = builder.moveToElement(we);
+            hoverOver.perform();
+
+            //TODO Add logic to click the one you want.
+        }
+    }
+
+    public String getDistance(String resort) {
+        String result = "Something went wrong";
+        Actions builder = new Actions(driver);
+        Actions hoverOver = builder.moveToElement(resortsHover);
+        hoverOver.perform();
+        for (WebElement res : resortsList) {
+
+            if ((res.toString()).equalsIgnoreCase(resort)) {
+                res.click();
+                result = res.toString();
+                return result;
+            }
+        }
+
+        return result;
+
     }
 
 
--- src/main/java/tests/Tests.java
+++ src/main/java/tests/Tests.java
@@ -4,6 +4,7 @@ import base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
@@ -16,13 +17,27 @@ public class Tests extends TestBase {
     public void titleTest(){
         HomePage homePage = new HomePage(driver);
         System.out.println(homePage.getTitle().toString());
+        Assert.assertEquals("Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah", driver.getTitle());
     }
 
     @Test(priority=1)
     public void storiesTest(){
         HomePage homePage = new HomePage(driver);
-
         homePage.clickStories();
+        System.out.println(driver.getTitle().toString());
+        Assert.assertEquals("Read About the Latest Happenings on the Slopes - Ski Utah", driver.getTitle());
+    }
+
+    @Test(priority=2)
+    public void submenuTest(){
+        HomePage homePage = new HomePage(driver);
+        homePage.getSubMenu();
+    }
+
+    @Test(priority=3)
+    public void distanceTest(){
+        HomePage homePage = new HomePage(driver);
+        homePage.getDistance("snowbird");
     }
 
 
